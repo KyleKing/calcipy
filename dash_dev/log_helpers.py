1f956d498e3710d98fa097fff08416e6b5ad8653
Kyle King <KyleKing@users.noreply.github.com>
2020-11-13 00:03:57 -0500

    Chg: improve logger context manager

--- dash_dev/log_helpers.py
+++ dash_dev/log_helpers.py
@@ -3,6 +3,7 @@
 # PLANNED: Files like this should be in the production dependencies, but dash-dev is designed to be a dev-only
 
 import json
+import time
 from typing import Any, Dict
 
 from decorator import contextmanager
@@ -62,9 +63,20 @@ def serializable_compact(record: Dict[str, Any]) -> str:
 
 
 @contextmanager
-def logger_context(message: str, level: str = 'INFO', _logger: Any = logger) -> None:
+def logger_context(message: str, level: str = 'INFO', _logger: type(logger) = logger) -> None:
+    """Log the beggining and end of a context.
 
-    # TODO: Bind some sort of indicator to make these two related events easier to find?
-    _logger.log(level, f'Started: {message}', context=True)
+    Args:
+        message: string message to describe the context
+        level: log level. Default is `INFO`
+        _logger: Optional logger instance
+
+    Yields:
+        yields to context
+
+    """
+    context_id = time.time_ns()
+    _logger.log(level, f'Started: {message}', context_id=context_id)
     yield
-    _logger.log(level, f'Completed: {message}', context=False)
+    runtime = time.time_ns() - context_id
+    _logger.log(level, f'In {runtime:.2f} seconds, completed: {message}', context_id=context_id, runtime=runtime)
