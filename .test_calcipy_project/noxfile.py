297035bd6c904999678f311ba08869b82a498317
Kyle King <KyleKing@users.noreply.github.com>
2021-05-26 21:46:38 -0400

    build: update template repository

--- .test_calcipy_project/noxfile.py
+++ .test_calcipy_project/noxfile.py
@@ -7,7 +7,7 @@ from loguru import logger
 from nox_poetry import session
 from nox_poetry.sessions import Session
 
-from calcipy.doit_tasks.doit_globals import DIG
+from calcipy.doit_tasks.doit_globals import DG
 
 
 def configure_nox() -> None:
@@ -16,15 +16,30 @@ def configure_nox() -> None:
     nox.options.error_on_missing_interpreters = True
 
 
-@session(python=[DIG.test.pythons], reuse_venv=True)
+@session(python=[DG.test.pythons], reuse_venv=True)
 def tests(session: Session) -> None:
-    """Run doit test task for specified python versions."""
+    """Run doit test task for specified python versions.
+
+    Args:
+        session: nox_poetry Session
+
+    """
     session.install('.[dev]', '.[test]')
     session.run(*shlex.split('poetry run doit run test'))
 
 
-@session(python=[DIG.test.pythons[-1]], reuse_venv=False)
+@session(python=[DG.test.pythons[-1]], reuse_venv=False)
 def build(session: Session) -> None:
-    """Build the project files within a controlled environment for repeatability."""
+    """Build the project files within a controlled environment for repeatability.
+
+    Args:
+        session: nox_poetry Session
+
+    """
     path_wheel = session.poetry.build_package()
     logger.info(path_wheel)
+
+
+# PLANNED: Make an environment to check the built wheel file for imports once most are optional:
+#   https://stackoverflow.com/questions/34855071/importing-all-functions-from-a-package-from-import
+# PLANNED: Add environment for safety, see https://github.com/KyleKing/calcipy/issues/32 for snippet
