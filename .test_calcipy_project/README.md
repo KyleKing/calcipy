25fcc8345921bdc7ea9f6234638d48f4560c7bc8
Kyle King <KyleKing@users.noreply.github.com>
2021-06-05 17:53:05 -0400

    build: apply 0.0.10 template to .test directory

--- .test_calcipy_project/README.md
+++ .test_calcipy_project/README.md
@@ -1,7 +1,5 @@
-# Test Project
+# Test Project (test_project)
 
-Test files created with `calcipy_template` for use in testing `calcipy`
+This project is testing the functionality of copier_calcipy
 
 Full documentation, here: [./docs](./docs)
-
-<!-- FIXME: Actually delete the git directory... Causes issues if files aren't committed and not necessary on AppVeyor? -->
--- .test_calcipy_project/dodo.py
+++ .test_calcipy_project/dodo.py
@@ -18,11 +18,11 @@ poetry run doit
 
 """
 
-from test_project import __pkg_name__
-
 from calcipy.doit_tasks import *  # noqa: F401,F403,H303 (Run 'doit list' to see tasks). skipcq: PYL-W0614
 from calcipy.doit_tasks import DOIT_CONFIG_RECOMMENDED
 from calcipy.log_helpers import activate_debug_logging
+
+from test_project import __pkg_name__
 
 activate_debug_logging(pkg_names=[__pkg_name__])
 
