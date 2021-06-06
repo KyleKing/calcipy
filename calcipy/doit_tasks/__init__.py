2b705bb4e455665ca2f73477a803e8b5929b20cc
Kyle King <KyleKing@users.noreply.github.com>
2021-06-06 10:39:22 -0400

    refactor: see if only one space is okay for skipcq

--- calcipy/doit_tasks/__init__.py
+++ calcipy/doit_tasks/__init__.py
@@ -2,7 +2,7 @@
 
 Register all defaults doit tasks in a dodo.py file with the below snippet:
 
-`from calcipy.doit_tasks import *  # noqa: F401,F403,H303 (Run 'doit list' to see tasks).  # skipcq: PYL-W0614`
+`from calcipy.doit_tasks import *  # noqa: F401,F403,H303 (Run 'doit list' to see tasks). # skipcq: PYL-W0614`
 
 """
 
