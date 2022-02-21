51ed25b60583bc08ce89d4ba98c84b6bff50404f
Kyle King <KyleKing@users.noreply.github.com>
2022-02-21 18:51:08 -0500

    test: update for new tables.js file

--- tests/test_file_search.py
+++ tests/test_file_search.py
@@ -9,6 +9,6 @@ def test_find_project_files_by_suffix():
     result = find_project_files_by_suffix(DG.meta.path_project, DG.meta.ignore_patterns)
 
     assert len(result) != 0, f'Error: see {DG.meta.path_project}/README.md for configuring the directory'
-    assert sorted(result.keys()) == ['', 'ini', 'json', 'lock', 'md', 'py', 'toml', 'yaml', 'yml']
+    assert sorted(result.keys()) == ['', 'ini', 'js', 'json', 'lock', 'md', 'py', 'toml', 'yaml', 'yml']
     assert result[''][0].name == '.flake8'
     assert result['md'][-1].relative_to(DG.meta.path_project).as_posix() == 'tests/data/README.md'
