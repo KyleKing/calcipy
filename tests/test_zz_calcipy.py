272fbb7655a1993406555429dc6c9a1f0e2b97d8
Kyle King <KyleKing@users.noreply.github.com>
2023-02-08 20:07:13 -0500

    build: copier update

--- tests/test_zz_calcipy.py
+++ tests/test_zz_calcipy.py
@@ -12,7 +12,7 @@ except ModuleNotFoundError:
 
 def test_version():
     """Check that PyProject and __version__ are equivalent."""
-    data = Path('pyproject.toml').read_text()
+    data = Path('pyproject.toml').read_text(encoding='utf-8')
 
     result = tomllib.loads(data)['tool']['poetry']['version']
 
