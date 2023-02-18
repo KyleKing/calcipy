e3b13880c27268027c9e437d324e5cfa13f56bc9
Kyle King <KyleKing@users.noreply.github.com>
2023-02-17 20:04:43 -0500

    fix: properly map CLI arguments to code tag collector

--- calcipy/tasks/tags.py
+++ calcipy/tasks/tags.py
@@ -5,14 +5,12 @@ from pathlib import Path
 
 from beartype.typing import Optional
 from invoke import Context
-from shoal import get_logger
 from shoal.cli import task
 
-from ..code_tag_collector import CODE_TAG_RE, COMMON_CODE_TAGS, write_code_tag_file
+from ..code_tag_collector import write_code_tag_file
 from ..file_search import find_project_files
 from .defaults import from_ctx
 
-logger = get_logger()
 
 @task(  # type: ignore[misc]
     default=True,
@@ -29,7 +27,7 @@ def collect_code_tags(
         base_dir: str = '.',
         filename: Optional[str] = None,
         tag_order: str = '',
-        regex: str = CODE_TAG_RE,
+        regex: str = '',
         ignore_patterns: str = '',
     ) -> None:
     """Create a `CODE_TAG_SUMMARY.md` with a table for TODO- and FIXME-style code comments."""
@@ -37,14 +35,12 @@ def collect_code_tags(
     path_tag_summary = Path(filename or from_ctx(ctx, 'tags', 'filename')).resolve()
     patterns = ignore_patterns.split(',') if ignore_patterns else []
     paths_source = find_project_files(pth_base_dir, ignore_patterns=patterns)
-    regex_compiled = re.compile(regex.format(tag='|'.join(tag_order)))
 
     write_code_tag_file(
         path_tag_summary=path_tag_summary,
         paths_source=paths_source,
         base_dir=pth_base_dir,
-        regex_compiled=regex_compiled,
-        tag_order=tag_order.split(',') or COMMON_CODE_TAGS,
+        regex=regex,
+        tags=tag_order,
         header='# Collected Code Tags',
     )
-    logger.info('Created Code Tag Summary', path_tag_summary=path_tag_summary)
