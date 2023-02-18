c5eda55f92ed8fe1e97f7527be7ce992eb42675e
Kyle King <KyleKing@users.noreply.github.com>
2023-02-18 05:59:40 -0500

    build: switch to extras and drop lxml

--- calcipy/tasks/types.py
+++ calcipy/tasks/types.py
@@ -1,15 +1,11 @@
 """Types CLI."""
 
-from pathlib import Path
-
 from beartype import beartype
-from beartype.typing import Optional
 from invoke import Context
 from shoal import get_logger
 from shoal.cli import task
 
-from ..file_helpers import open_in_browser, read_package_name
-from .defaults import from_ctx
+from ..file_helpers import read_package_name
 
 logger = get_logger()
 
@@ -29,17 +25,8 @@ def pyright(ctx: Context) -> None:
     """Run pyright."""
     _inner_task(ctx, cli_args='', command='pyright')
 
-@task(  # type: ignore[misc]
-    help={
-        'out_dir': 'Optional path to the report directory. Typically "releases/tests/mypy_html"',
-        'view': 'If True, open the created file',
-    },
-)
-def mypy(ctx: Context, *, out_dir: Optional[str] = None, view: bool = False) -> None:
-    """Run mypy."""
-    # PLANNED: If `out_dir == ''`, then do not create an HTML report
-    report_dir = Path(out_dir or from_ctx(ctx, 'types', 'out_dir'))
-    _inner_task(ctx, cli_args=f' --html-report={report_dir}', command='python -m mypy')
 
-    if view:
-        open_in_browser(report_dir / 'index.html')
+@task()  # type: ignore[misc]
+def mypy(ctx: Context) -> None:
+    """Run mypy."""
+    _inner_task(ctx, cli_args='', command='python -m mypy')
