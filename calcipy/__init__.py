2aa143da78f01fa9171aaa59a96fed39b77f14d2
Kyle King <KyleKing@users.noreply.github.com>
2023-02-20 21:56:29 -0500

    build: update with copier

--- calcipy/__init__.py
+++ calcipy/__init__.py
@@ -1,6 +1,6 @@
 """calcipy."""
 
-__version__ = '1.0.0'
+__version__ = '0.21.5'
 __pkg_name__ = 'calcipy'
 
 # ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
