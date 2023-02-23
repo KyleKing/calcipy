02a6ae2ed95988848916190d5f0f0b78125eba6a
Kyle King <KyleKing@users.noreply.github.com>
2023-02-22 21:19:43 -0500

    bump: version 1.0.0rc4 → 1.0.0rc5

--- calcipy/__init__.py
+++ calcipy/__init__.py
@@ -1,6 +1,6 @@
 """calcipy."""
 
-__version__ = '1.0.0rc4'
+__version__ = '1.0.0rc5'
 __pkg_name__ = 'calcipy'
 
 # ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
