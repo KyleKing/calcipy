9ebbb7bddf1acf824efc73a527db296b8524f9a8
Kyle King <KyleKing@users.noreply.github.com>
2023-04-06 19:44:47 -0400

    fix: remove lru_cache, which was causing mypy errors

--- calcipy/invoke_helpers.py
+++ calcipy/invoke_helpers.py
@@ -39,7 +39,6 @@ def run(ctx: Context, *run_args: Any, **run_kwargs: Any) -> Result:
 # Invoke Task Helpers
 
 
-@lru_cache(maxsize=1)
 @beartype
 def get_project_path() -> Path:
     """Retrieve the `cwd`."""
