be94549a68a9c901f814cd2174d46a0018e81968
Kyle King <KyleKing@users.noreply.github.com>
2023-08-28 21:56:20 -0400

    bump: version 1.6.3 → 1.6.4

--- calcipy/__init__.py
+++ calcipy/__init__.py
@@ -10,7 +10,7 @@ from beartype.claw import beartype_this_package
 from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
 from typing_extensions import Self  # noqa: UP035
 
-__version__ = '1.6.3'
+__version__ = '1.6.4'
 __pkg_name__ = 'calcipy'
 
 
