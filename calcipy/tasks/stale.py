089505096a9bd7ecd6491dfd885e6127d10455df
Kyle King <KyleKing@users.noreply.github.com>
2024-06-06 22:34:48 -0400

    fix: remove pip-check

--- calcipy/tasks/stale.py
+++ calcipy/tasks/stale.py
@@ -4,8 +4,6 @@ from invoke.context import Context
 
 from ..check_for_stale_packages import check_for_stale_packages as cfsp
 from ..cli import task
-from ..invoke_helpers import run
-from .executable_utils import python_dir
 
 
 @task(
@@ -14,7 +12,6 @@ from .executable_utils import python_dir
         'stale_months': 'Cutoff in months for when a package may be stale enough to be a risk',
     },
 )
-def check_for_stale_packages(ctx: Context, *, stale_months: int = 48) -> None:
+def check_for_stale_packages(ctx: Context, *, stale_months: int = 48) -> None:  # noqa: ARG001
     """Identify stale dependencies."""
     cfsp(stale_months=stale_months)
-    run(ctx, f'{python_dir()}/pip-check --cmd="poetry run pip" --hide-unchanged')
