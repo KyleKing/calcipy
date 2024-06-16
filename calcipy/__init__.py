393ce894a4b7f643e13fb290a0eaabfb86f706f0
Kyle King <KyleKing@users.noreply.github.com>
2024-06-16 16:30:04 -0400

    bump: version 3.0.1 → 3.0.2

--- calcipy/__init__.py
+++ calcipy/__init__.py
@@ -2,7 +2,7 @@
 
 from ._runtime_type_check_setup import configure_runtime_type_checking_mode
 
-__version__ = '3.0.1'
+__version__ = '3.0.2'
 __pkg_name__ = 'calcipy'
 
 configure_runtime_type_checking_mode()
