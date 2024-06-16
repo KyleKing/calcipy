24b047c1cce2a640ef14f79a619c0553e986522f
Kyle King <KyleKing@users.noreply.github.com>
2024-06-16 16:29:52 -0400

    build: copier-auto-update

--- calcipy/__init__.py
+++ calcipy/__init__.py
@@ -1,55 +1,11 @@
 """calcipy."""
 
-from datetime import datetime, timezone
-from enum import Enum
-from os import getenv
-from warnings import filterwarnings
-
-from beartype import BeartypeConf
-from beartype.claw import beartype_this_package
-from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
-from typing_extensions import Self
+from ._runtime_type_check_setup import configure_runtime_type_checking_mode
 
 __version__ = '3.0.1'
 __pkg_name__ = 'calcipy'
 
-
-class _RuntimeTypeCheckingModes(Enum):
-    """Supported global runtime type checking modes."""
-
-    ERROR = 'ERROR'
-    WARNING = 'WARNING'
-    OFF = None
-
-    @classmethod
-    def from_environment(cls) -> Self:  # pragma: no cover
-        """Return the configured mode."""
-        rtc_mode = getenv('RUNTIME_TYPE_CHECKING_MODE') or None
-        try:
-            return cls(rtc_mode)
-        except ValueError:
-            modes = [_e.value for _e in cls]
-            msg = f"'RUNTIME_TYPE_CHECKING_MODE={rtc_mode}' is not an allowed mode from {modes}"
-            raise ValueError(msg) from None
-
-
-def configure_runtime_type_checking_mode() -> None:  # pragma: no cover
-    """Optionally configure runtime type checking mode globally."""
-    rtc_mode = _RuntimeTypeCheckingModes.from_environment()
-
-    if rtc_mode is not _RuntimeTypeCheckingModes.OFF:
-        from beartype.roar import BeartypeClawDecorWarning  # noqa: PLC0415
-
-        beartype_this_package(conf=BeartypeConf(
-            warning_cls_on_decorator_exception=(
-                None if rtc_mode is _RuntimeTypeCheckingModes.ERROR else BeartypeClawDecorWarning
-            ),
-        ))
-
-
-_PEP585_DATE = 2025
-if datetime.now(tz=timezone.utc).year <= _PEP585_DATE:  # pragma: no cover
-    filterwarnings('ignore', category=BeartypeDecorHintPep585DeprecationWarning)
 configure_runtime_type_checking_mode()
 
-# ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
+
+# == Above code must always be first ==
---
+++ calcipy/_runtime_type_check_setup.py
@@ -0,0 +1,56 @@
+"""Configure runtime typechecking conditionally."""
+
+from datetime import datetime, timezone
+from enum import Enum
+from os import getenv
+from warnings import filterwarnings
+
+from beartype import BeartypeConf
+from beartype.claw import beartype_this_package
+from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
+from typing_extensions import Self
+
+
+class _RuntimeTypeCheckingModes(Enum):
+    """Supported global runtime type checking modes."""
+
+    ERROR = 'ERROR'
+    WARNING = 'WARNING'
+    OFF = None
+
+    @classmethod
+    def from_environment(cls) -> Self:  # pragma: no cover
+        """Return the configured mode."""
+        rtc_mode = getenv('RUNTIME_TYPE_CHECKING_MODE') or None
+        try:
+            return cls(rtc_mode)
+        except ValueError:
+            modes = [_e.value for _e in cls]
+            msg = f"'RUNTIME_TYPE_CHECKING_MODE={rtc_mode}' is not from {modes}"
+            raise ValueError(msg) from None
+
+
+def configure_runtime_type_checking_mode() -> None:  # pragma: no cover
+    """Optionally configure runtime type checking mode globally."""
+    rtc_mode = _RuntimeTypeCheckingModes.from_environment()
+
+    if rtc_mode is not _RuntimeTypeCheckingModes.OFF:
+        from beartype.roar import BeartypeClawDecorWarning  # noqa: PLC0415
+
+        beartype_this_package(
+            conf=BeartypeConf(
+                warning_cls_on_decorator_exception=(
+                    None
+                    if rtc_mode is _RuntimeTypeCheckingModes.ERROR
+                    else BeartypeClawDecorWarning
+                ),
+            ),
+        )
+
+
+_PEP585_DATE = 2025
+if datetime.now(tz=timezone.utc).year <= _PEP585_DATE:  # pragma: no cover
+    filterwarnings(
+        'ignore',
+        category=BeartypeDecorHintPep585DeprecationWarning,
+    )
