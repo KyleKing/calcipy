dad1f0fcb0806bf8cdab7ace485565ef36193ce9
Kyle King <KyleKing@users.noreply.github.com>
2024-07-05 08:50:12 -0400

    fix: correctly calculate markdown table widths

--- calcipy/_md_helpers.py
+++ calcipy/_md_helpers.py
@@ -6,7 +6,7 @@ from beartype.typing import Any, Dict, List
 def _format_md_table(headers: List[str], records: List[Dict[str, Any]]) -> List[str]:
     """Format the input as a Github markdown table."""
     table = [[str(_r[col]) for col in headers] for _r in records]
-    widths = [max(len(row[col_idx]) for row in [headers, *table]) for col_idx in range(len(headers))]
+    widths = [max(len(row[col_idx].strip()) for row in [headers, *table]) for col_idx in range(len(headers))]
 
     def pad(values: List[str]) -> List[str]:
         return [val.strip().ljust(widths[col_idx]) for col_idx, val in enumerate(values)]
