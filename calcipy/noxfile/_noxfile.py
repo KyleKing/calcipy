d644f5d304fd1ff739503040eef33d3dcedf59a1
Kyle King <KyleKing@users.noreply.github.com>
2024-11-07 07:34:19 -0500

    feat!: remove nox build and check

--- calcipy/noxfile/_noxfile.py
+++ calcipy/noxfile/_noxfile.py
@@ -1,17 +1,17 @@
-"""nox-poetry configuration file.
+"""nox-uv configuration file.
 
 [Useful snippets from docs](https://nox.thea.codes/en/stable/usage.html)
 
 ```sh
-poetry run nox -l
-poetry run nox --list-sessions
+uv run nox -l
+uv run nox --list-sessions
 
-poetry run nox -s build_check-3.8 build_dist-3.8 tests-3.8
-poetry run nox --session tests-3.11
+uv run nox -s build_check-3.8 build_dist-3.8 tests-3.8
+uv run nox --session tests-3.11
 
-poetry run nox --python 3.8
+uv run nox --python 3.8
 
-poetry run nox -k "not build_check and not build_dist"
+uv run nox -k "not build_check and not build_dist"
 ```
 
 Useful nox snippets
@@ -34,31 +34,25 @@ with open(path_stdout, 'w') as out:
 
 import shlex
 from functools import lru_cache
-from pathlib import Path
-from urllib.parse import urlparse
-from urllib.request import url2pathname
 
 from beartype.typing import Dict, List, Union, cast
 from corallium import file_helpers  # Required for mocking read_pyproject
-from corallium.file_helpers import LOCK, get_tool_versions, if_found_unlink, read_package_name
-from corallium.log import LOGGER
+from corallium.file_helpers import LOCK, get_tool_versions, read_package_name
 from corallium.tomllib import tomllib
 from nox import Session as NoxSession
-from nox_poetry import session as nox_poetry_session
-from nox_poetry.poetry import DistributionFormat
-from nox_poetry.sessions import Session as NPSession
+from nox_uv.sessions import Session as NPSession
 
 if read_package_name() == 'corallium':
-    # 'poetry export' will fail on circular dependencies, so use nox instead of nox-poetry
+    # 'uv export' will fail on circular dependencies, so use nox instead of nox-uv
     from nox import session as nox_session
 else:
-    from nox_poetry import session as nox_session  # type: ignore[no-redef]
+    from nox_uv import session as nox_session  # type: ignore[no-redef]
 
 
 @lru_cache(maxsize=1)
 def _get_pythons() -> List[str]:
     """Return python versions from the `.tool-versions` file."""
-    # TODO: interop for uv/poetry/* && asdf/mise
+    # TODO: interop for uv/uv/* && asdf/mise
     return [*{str(ver) for ver in get_tool_versions()['python']}]
 
 
@@ -71,9 +65,9 @@ def _retrieve_keys(source: Dict, keys: List[str]) -> Dict:  # type: ignore[type-
     return result
 
 
-def _get_poetry_dev_dependencies() -> Dict[str, Dict]:  # type: ignore[type-arg]
+def _get_uv_dev_dependencies() -> Dict[str, Dict]:  # type: ignore[type-arg]
     """Return a dictionary of all dev-dependencies from the 'pyproject.toml'."""
-    poetry_config = file_helpers.read_pyproject()['tool']['poetry']
+    uv_config = file_helpers.read_pyproject()['tool']['uv']
 
     def normalize_dep(value: Union[str, Dict]) -> Dict:  # type: ignore[type-arg]
         return {'version': value} if isinstance(value, str) else value
@@ -81,8 +75,8 @@ def _get_poetry_dev_dependencies() -> Dict[str, Dict]:  # type: ignore[type-arg]
     return {
         key: normalize_dep(value)
         for key, value in {
-            **_retrieve_keys(poetry_config, ['dev', 'dependencies']),
-            **_retrieve_keys(poetry_config, ['group', 'dev', 'dependencies']),
+            **_retrieve_keys(uv_config, ['dev', 'dependencies']),
+            **_retrieve_keys(uv_config, ['group', 'dev', 'dependencies']),
         }.items()
     }
 
@@ -105,7 +99,7 @@ def _installable_dev_dependencies() -> List[str]:
 
     return [
         f'{to_package(key, value)}{to_constraint(value)}'
-        for key, value in _get_poetry_dev_dependencies().items()
+        for key, value in _get_uv_dev_dependencies().items()
         if key != 'calcipy'
     ]
 
@@ -113,13 +107,13 @@ def _installable_dev_dependencies() -> List[str]:
 def _install_local(session: Union[NoxSession, NPSession]) -> None:  # pragma: no cover
     """Ensure local dev-dependencies and calcipy extras are installed.
 
-    See: https://github.com/cjolowicz/nox-poetry/issues/230#issuecomment-855445920
+    See: https://github.com/cjolowicz/nox-uv/issues/230#issuecomment-855445920
 
     """
     if read_package_name() == 'calcipy':
         session = cast(NPSession, session)
         lock_data = tomllib.loads(LOCK.read_text())
-        session.poetry.installroot(extras=[*lock_data['extras']])  # First party extras
+        session.uv.installroot(extras=[*lock_data['extras']])  # First party extras
     else:
         extras = ['test']
         session.install('.', f'calcipy[{",".join(extras)}]')
@@ -133,37 +127,3 @@ def tests(session: Union[NoxSession, NPSession]) -> None:  # pragma: no cover
     """Run doit test task for specified python versions."""
     _install_local(session)
     session.run(*shlex.split('pytest ./tests'), stdout=True, env={'RUNTIME_TYPE_CHECKING_MODE': 'WARNING'})
-
-
-@nox_session(python=_get_pythons()[-1:], reuse_venv=False)
-def build_dist(session: Union[NoxSession, NPSession]) -> None:  # pragma: no cover
-    """Build and test the project files within a controlled environment for repeatability."""
-    dist_path = Path('dist')
-    if_found_unlink(dist_path)
-
-    # Support 'corallium' by re-implementing "session.poetry.build_package()", from:
-    # https://github.com/cjolowicz/nox-poetry/blob/5772b66ebff8d5a3351a08ed402d3d31e48be5f8/src/nox_poetry/sessions.py#L233-L255
-    # https://github.com/cjolowicz/nox-poetry/blob/5772b66ebff8d5a3351a08ed402d3d31e48be5f8/src/nox_poetry/poetry.py#L111-L154
-    output = session.run(*shlex.split('poetry build --format=wheel --no-ansi'), external=True, silent=True)
-    output = cast(str, output)
-    wheel = dist_path / output.split()[-1]
-    path_wheel = wheel.resolve().as_uri()
-
-    LOGGER.text('Created wheel', path_wheel=path_wheel)
-    # Install the wheel and check that imports without any of the optional dependencies
-    session.install(path_wheel)
-    session.run(*shlex.split('python scripts/check_imports.py'), stdout=True)
-
-
-@nox_poetry_session(python=_get_pythons()[-1:], reuse_venv=True)
-def build_check(session: NPSession) -> None:  # pragma: no cover
-    """Check that the built output meets all checks."""
-    # Build sdist and fix return URI, which will have file://...#egg=calcipy
-    sdist_uri = session.poetry.build_package(distribution_format=DistributionFormat.SDIST)
-    path_sdist = Path(url2pathname(urlparse(sdist_uri).path))
-    LOGGER.text_debug('Fixed sdist URI', sdist_uri=sdist_uri, path_sdist=path_sdist)
-    # Check with pyroma
-    session.install('pyroma>=4.0', '--upgrade')
-    # required for "poetry.core.masonry.api" build backend
-    session.run('python', '-m', 'pip', 'install', 'poetry>=1.3', stdout=True)
-    session.run('pyroma', '--file', path_sdist.as_posix(), '--min=9', stdout=True)
--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
-"""nox-poetry configuration file."""
+"""nox-uv configuration file."""
 
-from calcipy.noxfile import build_check, build_dist, tests  # noqa: F401
+from calcipy.noxfile import tests  # noqa: F401
 
 # Investigate: https://github.com/MtkN1/nox-uv-demo/blob/338007b4c4fdb7ceb479061999a48d86905af7b1/noxfile.py
 #   and: https://nox.thea.codes/en/stable/config.html
