bb8b7dc3eb5c96216f30af4026ff13d555fb378c
Kyle King <KyleKing@users.noreply.github.com>
2026-01-18 22:07:33 -0600

    bump: version 5.0.0 → 6.0.0b0

--- src/calcipy/__init__.py
+++ src/calcipy/__init__.py
@@ -2,7 +2,7 @@
 
 from ._runtime_type_check_setup import configure_runtime_type_checking_mode
 
-__version__ = '5.0.0'
+__version__ = '6.0.0b0'
 __pkg_name__ = 'calcipy'
 
 configure_runtime_type_checking_mode()
