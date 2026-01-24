51e09bd7401c69d18afaa056129f86d8cc98c1ef
Kyle King <KyleKing@users.noreply.github.com>
2026-01-24 06:36:24 -0600

    hack: temporarily patch nox dependency on corallium

--- calcipy/noxfile/_noxfile.py
+++ calcipy/noxfile/_noxfile.py
@@ -29,6 +29,7 @@ from functools import lru_cache
 
 from beartype.typing import Any, Dict, List, Union
 from corallium.file_helpers import get_tool_versions, read_pyproject
+from corallium.log import LOGGER
 from nox import Session as NoxSession
 from nox import session as nox_session
 
@@ -36,7 +37,11 @@ from nox import session as nox_session
 @lru_cache(maxsize=1)
 def _get_pythons() -> List[str]:
     """Return python versions from supported configuration files."""
-    return [*{str(ver) for ver in get_tool_versions()['python']}]
+    try:
+        return [*{str(ver) for ver in get_tool_versions()['python']}]
+    except Exception:
+        LOGGER.exception('Failed to retrieve tool versions. Skipping tasks')
+        return []
 
 
 def _has_ci_group(pyproject_data: Union[Dict[str, Any], None] = None) -> bool:
