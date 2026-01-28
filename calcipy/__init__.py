f26eb8b2ac9b1622003ff148fb74669e5f63f86f
Kyle King <KyleKing@users.noreply.github.com>
2026-01-28 00:16:17 -0600

    bump: version 6.0.0b0 → 6.0.0b1

--- calcipy/__init__.py
+++ calcipy/__init__.py
@@ -2,7 +2,7 @@
 
 from ._runtime_type_check_setup import configure_runtime_type_checking_mode
 
-__version__ = '6.0.0b0'
+__version__ = '6.0.0b1'
 __pkg_name__ = 'calcipy'  # noqa: RUF067
 
 configure_runtime_type_checking_mode()  # noqa: RUF067
